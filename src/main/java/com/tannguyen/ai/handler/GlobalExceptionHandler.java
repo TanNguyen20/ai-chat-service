f2f401931f7cca634dd6a3897ff82559bfe65086
Tan Nguyen <82394019+tannguyen20@users.noreply.github.com>
2025-08-05 10:46:13 +0700

    handling conflict resource

--- src/main/java/com/tannguyen/ai/handler/GlobalExceptionHandler.java
+++ src/main/java/com/tannguyen/ai/handler/GlobalExceptionHandler.java
@@ -2,35 +2,37 @@ package com.tannguyen.ai.handler;
 
 import com.tannguyen.ai.dto.response.ResponseFactory;
 import com.tannguyen.ai.dto.response.StandardResponse;
+import com.tannguyen.ai.exception.ForbiddenException;
 import com.tannguyen.ai.exception.NotFoundException;
 import com.tannguyen.ai.exception.ResourceAlreadyExistsException;
+import com.tannguyen.ai.exception.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<?> handleNotFoundException(UnauthorizedException ex) {
+        return ResponseFactory.error(HttpStatus.UNAUTHORIZED, "Login is required for access this data", ex.getMessage());
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<?> handleNotFoundException(ForbiddenException ex) {
+        return ResponseFactory.error(HttpStatus.FORBIDDEN, "You are not allow to access this data", ex.getMessage());
+    }
+
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<?> handleNotFoundException(NotFoundException ex) {
-        return ResponseFactory.error(HttpStatus.NOT_FOUND, "Internal server error", ex.getMessage());
+        return ResponseFactory.error(HttpStatus.NOT_FOUND, "Data not found", ex.getMessage());
     }
 
     @ExceptionHandler(ResourceAlreadyExistsException.class)
     public ResponseEntity<?> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.CONFLICT.value());
-        body.put("error", "Existed");
-        body.put("message", ex.getMessage());
-
-        return new ResponseEntity<>(body, HttpStatus.CONFLICT);
+        return ResponseFactory.error(HttpStatus.CONFLICT, "Resource already existed", ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
