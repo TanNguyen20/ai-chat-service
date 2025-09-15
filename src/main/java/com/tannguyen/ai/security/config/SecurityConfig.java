f0bcf030ba52ba54ae217be5c1fac645ed3a9662
Tan Nguyen <82394019+TanNguyen20@users.noreply.github.com>
2025-09-15 11:14:40 +0700

    update: allow get page access list without login

--- src/main/java/com/tannguyen/ai/security/config/SecurityConfig.java
+++ src/main/java/com/tannguyen/ai/security/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 API_V1 + "/auth/**",
                                 API_V1 + "/chatbot/info",
+                                API_V1 + "/pages",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
