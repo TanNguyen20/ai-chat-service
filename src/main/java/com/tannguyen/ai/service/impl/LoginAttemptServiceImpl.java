10b8109fb34303a71b89b9855fda94dde9471621
Tan Nguyen <82394019+TanNguyen20@users.noreply.github.com>
2025-09-25 10:28:37 +0700

    Update LoginAttemptServiceImpl.java

--- src/main/java/com/tannguyen/ai/service/impl/LoginAttemptServiceImpl.java
+++ src/main/java/com/tannguyen/ai/service/impl/LoginAttemptServiceImpl.java
@@ -7,6 +7,7 @@ import com.tannguyen.ai.service.inf.LoginAttemptService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
