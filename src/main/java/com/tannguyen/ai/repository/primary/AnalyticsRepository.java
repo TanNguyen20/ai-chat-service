dbb429db26b9567d122411c8a263a35c54c5a596
Tan Nguyen <82394019+TanNguyen20@users.noreply.github.com>
2025-09-29 09:56:23 +0700

    Update AnalyticsRepository.java

--- src/main/java/com/tannguyen/ai/repository/primary/AnalyticsRepository.java
+++ src/main/java/com/tannguyen/ai/repository/primary/AnalyticsRepository.java
@@ -4,10 +4,14 @@ import com.tannguyen.ai.model.primary.Analytics;
 import com.tannguyen.ai.model.primary.Role;
 import com.tannguyen.ai.model.primary.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface AnalyticsRepository extends JpaRepository<Analytics, Long> {
     List<Analytics> findAllByUsersContainingOrRolesIn(User user, Collection<Role> roles);
+
+    Page<Analytics> findAllByUsersContainingOrRolesIn(User user, Set<Role> roles, Pageable pageable);
 }
