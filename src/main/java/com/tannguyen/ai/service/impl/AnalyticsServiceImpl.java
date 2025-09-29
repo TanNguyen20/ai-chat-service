e561f518cb9cfee5d53597963fcf17b798a476cf
Tan Nguyen <82394019+TanNguyen20@users.noreply.github.com>
2025-09-29 09:53:57 +0700

    Update AnalyticsServiceImpl.java

--- src/main/java/com/tannguyen/ai/service/impl/AnalyticsServiceImpl.java
+++ src/main/java/com/tannguyen/ai/service/impl/AnalyticsServiceImpl.java
@@ -15,7 +15,7 @@ import com.tannguyen.ai.service.inf.AnalyticsService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
