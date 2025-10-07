5a40d6b3f80bfdeb1d54d655f906aa3afdd7303c
Tan Nguyen <82394019+TanNguyen20@users.noreply.github.com>
2025-10-07 09:07:10 +0700

    fix: missing inject passwordEncode to user service

--- src/main/java/com/tannguyen/ai/service/impl/UserServiceImpl.java
+++ src/main/java/com/tannguyen/ai/service/impl/UserServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 import java.util.Objects;
@@ -26,6 +27,7 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
 
     public Page<UserResponseDTO> getAllUsers(Pageable pageable) {
         return userRepository.findAll(pageable)
