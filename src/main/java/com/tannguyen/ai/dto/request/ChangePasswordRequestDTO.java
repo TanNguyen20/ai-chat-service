86300a45f2fcfbeb79e23faf19f85b6247937ed2
Tan Nguyen <82394019+TanNguyen20@users.noreply.github.com>
2025-10-07 08:51:43 +0700

    update: add change password request dto

---
+++ src/main/java/com/tannguyen/ai/dto/request/ChangePasswordRequestDTO.java
@@ -0,0 +1,16 @@
+package com.tannguyen.ai.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ChangePasswordRequestDTO {
+    @NotBlank
+    private String currentPassword;
+
+    @NotBlank
+    private String newPassword;
+
+    @NotBlank
+    private String confirmNewPassword;
+}
